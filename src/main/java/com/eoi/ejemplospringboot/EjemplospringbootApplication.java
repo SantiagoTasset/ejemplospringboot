91b0fbfa1b51a262050ca5129618c3d56903cbe9
alejandro <mail.alejandro.teixeira@gmail.com>
2023-05-18 21:31:23 +0200

    Initial commit

---
+++ src/main/java/com/eoi/ejemplospringboot/EjemplospringbootApplication.java
@@ -0,0 +1,13 @@
+package com.eoi.ejemplospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplospringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplospringbootApplication.class, args);
+	}
+
+}
