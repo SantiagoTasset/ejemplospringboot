872b3678475742bf339afef2866c60cb99ad6510
alejandro <mail.alejandro.teixeira@gmail.com>
2023-05-18 23:14:13 +0200

    Añadidos comentarios en clases principales

--- src/main/java/com/eoi/ejemplospringboot/EjemplospringbootApplication.java
+++ src/main/java/com/eoi/ejemplospringboot/EjemplospringbootApplication.java
@@ -4,12 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la aplicación Spring Boot.
+ *
+ * La anotación @SpringBootApplication se utiliza para marcar la clase principal de la aplicación Spring Boot.
+ * Esta anotación combina múltiples anotaciones, como @Configuration, @EnableAutoConfiguration y @ComponentScan
+ * para proporcionar una configuración y arranque automáticos de la aplicación Spring Boot.
+ * Proporciona una forma conveniente de configurar y lanzar una aplicación Spring Boot sin la necesidad
+ * de una configuración manual exhaustiva.
+ *
+ */
 @SpringBootApplication
-@EnableAutoConfiguration
 public class EjemplospringbootApplication {
 
+	/**
+	 * Punto de entrada principal de la aplicación.
+	 *
+	 * @param args los argumentos de línea de comandos
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(EjemplospringbootApplication.class, args);
 	}
 
 }
+
--- src/test/java/com/eoi/ejemplospringboot/EjemplospringbootApplicationTests.java
+++ src/test/java/com/eoi/ejemplospringboot/EjemplospringbootApplicationTests.java
@@ -3,11 +3,29 @@ package com.eoi.ejemplospringboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Pruebas de la aplicación Spring Boot.
+ *
+ * La anotación @SpringBootTest se utiliza para marcar la clase de prueba como una prueba de integración de Spring Boot.
+ * Esta anotación permite cargar y configurar el contexto de la aplicación Spring Boot
+ * para realizar pruebas en un entorno similar al entorno de ejecución real. Proporciona un conjunto de
+ * características adicionales para simplificar las pruebas de integración, como la carga automática de la
+ * configuración de la aplicación y la inyección de dependencias.
+ *
+ */
 @SpringBootTest
 class EjemplospringbootApplicationTests {
 
+	/**
+	 * Prueba para verificar la carga del contexto de la aplicación. Al ejecutar esta prueba, Spring Boot cargará y
+	 * configurará el contexto de la aplicación.
+	 * Si el contexto se carga correctamente, la prueba se considera exitosa.
+	 * Esta prueba simple se utiliza a menudo como un punto de partida para asegurarse de que
+	 * la configuración básica de la aplicación es correcta.
+	 */
 	@Test
 	void contextLoads() {
+
 	}
 
 }
