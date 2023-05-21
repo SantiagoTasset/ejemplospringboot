16666ab5dcc621391f9fd944c7d2f52bcc47cf08
Profesor <mail.alejandro.teixeira@gmail.com>
2023-05-21 22:48:43 +0200

    Herencia de clases avanzada

--- src/main/java/com/eoi/ejemplospringboot/configuration/AppConfig.java
+++ src/main/java/com/eoi/ejemplospringboot/configuration/AppConfig.java
@@ -4,23 +4,23 @@ import com.eoi.ejemplospringboot.abstractcomponents.GenericServiceWithJPA;
 import com.eoi.ejemplospringboot.controllers.UsuarioController;
 import com.eoi.ejemplospringboot.entities.Usuario;
 import com.eoi.ejemplospringboot.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    Environment environment;
 
-    @Bean
     public UsuarioController usuarioController() {
-
-        String entityName = "usuario";
-        return new UsuarioController(entityName);
-
+        return new UsuarioController(environment.getProperty("{url.usuario}"));
     }
-
-
-
 
 
 }
